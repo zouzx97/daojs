696a0baec915e9a543b3cb827bbf4cb86b868ded
Wei Wei <lyweiwei@outlook.com>
2018-01-16 22:47:06 +0800

    Implement the webpack build

---
+++ server/src/build/build.js
@@ -0,0 +1,53 @@
+const _ = require('lodash');
+const {Volume} = require('memfs');
+const fs = require('fs');
+const {Union} = require('unionfs');
+const webpack = require('webpack');
+const path = require('path');
+fs.mkdirp = require('mkdirp');
+
+const SRC_FILE_PATH = 'memory-src-files';
+const DIST_FILE_PATH = 'memory-dist-files';
+const src = (name = '') => path.join(__dirname, SRC_FILE_PATH, name);
+const dist = (name = '') => path.join(__dirname, DIST_FILE_PATH, name);
+
+function build(entry, files) {
+  return new Promise((resolve, reject) => {
+    const ufs = new Union();
+    ufs
+      .use(fs)
+      .use(Volume.fromJSON(_.mapKeys(files, (content, name) => src(name))));
+    ufs.join = path.join;
+
+    const compiler = webpack({
+      entry: src(entry),
+      output: {
+        path: dist(),
+        filename: 'index.js',
+      },
+      module: {
+        rules: [{
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+          options: {
+            presets: ['env', 'react'],
+          },
+        }],
+      },
+    });
+    compiler.inputFileSystem = ufs;
+    compiler.resolvers.normal.fileSystem = compiler.inputFileSystem;
+    compiler.resolvers.context.fileSystem = compiler.inputFileSystem;
+    compiler.outputFileSystem = ufs;
+    compiler.run((err, status) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(ufs.readFileSync(dist('index.js'), 'utf8'));
+      }
+    });
+  });
+}
+
+module.exports = build;
--- server/src/build/index.js
+++ server/src/build/index.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const build = require('./build');
+
 const router = express.Router();
+const mockInput = `
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>Hello world!</div>,
+  document.getElementById('container')
+);
+`;
 
 router.get('/', function (req, res) {
-  res.send('Hello, build!');
+  build('index.js', {
+    'index.js': mockInput,
+  }).then((data) => {
+    res.send(data);
+  });
 });
 
 module.exports = router;
