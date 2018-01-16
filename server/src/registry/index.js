f80c949c8b93dea096e054bb6e0eb700ba5bf610
Wei Wei <lyweiwei@outlook.com>
2018-01-17 00:33:40 +0800

    Support upload

--- server/src/registry/index.js
+++ server/src/registry/index.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const {add} = require('../store');
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
+router.use(bodyParser.json());
 
+router.get('/', (req, res) => {
 });
 
 router.post('/:entry', (req, res) => {
-
+  add(req.params.entry, req.body);
+  res.send('OK');
 });
 
 module.exports = router;
