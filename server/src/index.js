e492b61798c490eee51f7727cdf51faaa9ea98db
Haipeng Wang <ericwa@microsoft.com>
2018-01-16 18:29:26 +0800

    add placeholder for registry

--- server/src/index.js
+++ server/src/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const registry = require('./registry/index');
 const app = express();
 
 app.get('/', function(req, res){
   res.send('hello world!');
 });
+
+app.route('/registry/:componentname/:version')
+.get(registry.get)
+.post(registry.post);
 
 app.listen(3000);
