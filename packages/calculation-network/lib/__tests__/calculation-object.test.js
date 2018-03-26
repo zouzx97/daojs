f4cb5fbe9cb8e0e205cbeddb23f3c70b1909cd44
Wei Wei <shazhouke@gmail.com>
2018-03-26 21:57:26 +0800

    Initialize the package

---
+++ packages/calculation-network/lib/__tests__/calculation-object.test.js
@@ -0,0 +1,7 @@
+const CalculationObject = require('../index.js');
+
+describe('CalculationObject', () => {
+  it('has a test', () => {
+    expect(CalculationObject).toBeInstanceOf(Function);
+  });
+});
