fe5e7d4d38e47729952be2ae97d2524e5e63f3dc
Wei Wei <shazhouke@gmail.com>
2018-04-09 15:31:05 +0800

    Add latest changes

--- projects/sodexo/new-src/worker/dag-ql.js
+++ projects/sodexo/new-src/worker/dag-ql.js
@@ -2,7 +2,9 @@ import { Loader } from 'calculation-network';
 import * as procedures from './procedures';
 import story from './story.yaml';
 
-const contextNetwork = new Loader(procedures).load(story);
+const contextNetwork = new Loader({
+  ...procedures,
+}).load(story);
 
 export async function set(key, value) {
   return (await contextNetwork).set({ [key]: value });
