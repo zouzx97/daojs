32948ac2d6cde4f2c856525d2518d1f329d9db6e
Yucong Jin <yucongj@microsoft.com>
2018-04-09 04:43:25 -0700

    init commit

---
+++ packages/engine/dag-ql.js
@@ -0,0 +1,15 @@
+import { Loader } from '@daojs/calculation-network';
+import * as procedures from './procedures';
+import story from './story.yaml';
+
+const contextNetwork = new Loader({
+  ...procedures,
+}).load(story);
+
+export async function set(key, value) {
+  return (await contextNetwork).set({ [key]: value });
+}
+
+export async function get(key) {
+  return (await contextNetwork).get(key);
+}
