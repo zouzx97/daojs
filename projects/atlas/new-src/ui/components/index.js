be870ccbf733b1067534de8bab6dab27e97f03d6
Wei Wei <shazhouke@gmail.com>
2018-04-10 20:37:27 +0800

    Rename async-registry to registry

--- projects/atlas/new-src/ui/components/index.js
+++ projects/atlas/new-src/ui/components/index.js
@@ -1,10 +1,10 @@
 import _ from 'lodash';
-import AsyncRegistry from '@daojs/async-registry';
+import Registry from '@daojs/registry';
 import SingleSelector from './single-selector';
 import PlainData from './plain-data';
 import Flexbox from './flexbox';
 
-export default new AsyncRegistry().register({
+export default new Registry().register({
   SingleSelector,
   PlainData,
   Flexbox,
