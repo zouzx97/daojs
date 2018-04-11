78fa2a0ab772879c07e3d65ce0263bc14042c724
Wei Wei <shazhouke@gmail.com>
2018-04-11 17:42:33 +0800

    Add the arithmetic test

--- packages/worker-rpc/test/index.test.js
+++ packages/worker-rpc/test/index.test.js
@@ -19,3 +19,26 @@ describe('Echo server', () => {
     expect(await client.echo(text)).to.equal(text);
   });
 });
+
+describe('Arithmetic server', () => {
+  const url = workerURL('arithmetic.js');
+  let client = null;
+
+  beforeEach(async () => {
+    client = await createClient(url);
+  });
+
+  it('should do string/array conversions correctly', async () => {
+    const str = '1234567';
+    const arr = [7, 6, 5, 4, 3, 2, 1];
+    expect(await client.string2array(str)).to.deep.equal(arr);
+    expect(await client.array2string(arr)).to.equal(str);
+  });
+
+  it('should calculate the add method correctly', async () => {
+    const strN = '12345678901234567890';
+    const str2N = '24691357802469135780';
+
+    expect(await client.add(strN, strN)).to.equal(str2N);
+  });
+});
