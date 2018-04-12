09b5e310261e2aa19ebd31cd956576061ebcb0f0
Zhibin Liu <ljqx@live.cn>
2018-04-12 18:06:49 +0800

    charts for dashboard

--- packages/botana-components/src/components/retention.js
+++ packages/botana-components/src/components/retention.js
@@ -1,7 +1,10 @@
+import components from '@daojs/builtin-components';
 import React from 'react';
+
+const { Heatmap } = components;
 
 export default function Retention(props) {
   return (
-    <span>{JSON.stringify(props)}</span>
+    <Heatmap {...props} />
   );
 }
