5ed1ae81d6ce09d8853b9872abca70d894f5dcbb
Zhibin Liu <ljqx@live.cn>
2018-04-12 17:35:22 +0800

    Bucket

--- packages/botana-components/src/components/bucket.js
+++ packages/botana-components/src/components/bucket.js
@@ -1,7 +1,25 @@
+import components from '@daojs/builtin-components';
+import _ from 'lodash';
+import PropTypes from 'prop-types';
 import React from 'react';
 
+const { Bar } = components;
+
 export default function Bucket(props) {
-  return (
-    <span>{JSON.stringify(props)}</span>
-  );
+  const axisDimension = props.axisDimensions[0];
+  const metricDimension = props.metricDimensions[0];
+
+  const data = _.map(props.source, item => ({
+    [axisDimension]: `${item[axisDimension]} ~ ${item[axisDimension] + props.binStep}`,
+    [metricDimension]: item[metricDimension],
+  }));
+
+  return <Bar source={data} {...props} />;
 }
+
+Bucket.propTypes = {
+  binStep: PropTypes.number.isRequired,
+  source: PropTypes.arrayOf(PropTypes.object).isRequired,
+  axisDimensions: PropTypes.arrayOf(PropTypes.string).isRequired,
+  metricDimensions: PropTypes.arrayOf(PropTypes.string).isRequired,
+};
