50ee67ebb196e3ee92ebec56ad5eb30a628f614e
HaipengWang <haip_wang@163.com>
2018-04-11 18:24:07 -0700

    add componentRegistry to context

--- packages/builtin-components/src/components/layout-default.js
+++ packages/builtin-components/src/components/layout-default.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes, { any } from 'prop-types';
 import { Map } from 'immutable';
+import ComponentRegistry from '@daojs/registry';
 import config2Cell from '../utils/config-to-cell';
 
 export default class LayoutDefault extends React.PureComponent {
@@ -9,6 +10,7 @@ export default class LayoutDefault extends React.PureComponent {
       read: key => this.props.data.get(key),
       isUpdating: key => this.props.isUpdating.get(key),
       update: this.props.update,
+      componentRegistry: this.props.componentRegistry,
     };
   }
 
@@ -22,4 +24,5 @@ LayoutDefault.propTypes = {
   data: PropTypes.instanceOf(Map).isRequired,
   isUpdating: PropTypes.instanceOf(Map).isRequired,
   update: PropTypes.func.isRequired,
+  componentRegistry: PropTypes.instanceOf(ComponentRegistry).isRequired,
 };
