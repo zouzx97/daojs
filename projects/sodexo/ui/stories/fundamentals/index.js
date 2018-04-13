4bce0e98b9a011b353385adbe6e555c64b02d230
Ling Zhou <lizho@microsoft.com>
2018-04-13 01:16:36 -0700

    Add draft

--- projects/sodexo/ui/stories/fundamentals/index.js
+++ projects/sodexo/ui/stories/fundamentals/index.js
@@ -1,9 +1,11 @@
 import dummy from './dummy.yaml';
 import calculate from './calculate.yaml';
 import testComponents from './test-components.yaml';
+import rechargeAndDepositFunds from './recharge-deposit-funds.yaml';
 
 export default [
   dummy,
   calculate,
   testComponents,
+  rechargeAndDepositFunds,
 ];
