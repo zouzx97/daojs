79ca06256949a47f483610fc55a17d203fd25004
suro1995 <lxdyran@qq.com>
2018-04-13 19:04:15 +0800

    props mapping for percentage chart

--- packages/botana-components/src/components/percentage.js
+++ packages/botana-components/src/components/percentage.js
@@ -1,12 +1,20 @@
 import components from '@daojs/builtin-components';
 import React from 'react';
+import _ from 'lodash';
 
 const { Donut } = components;
 
 export default function Percentage(props) {
+  // convert array-like object to array
+  const source = _.reduce(
+    props,
+    (memo, value, key) => (_.isFinite(key - 0) ? memo.concat(value) : memo),
+    [],
+  );
+
   return (
     <Donut
-      source={props}
+      source={source}
     />
   );
 }
