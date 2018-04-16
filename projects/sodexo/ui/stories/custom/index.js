2196b528de58d0ad487de0ee908f0d4385389f63
Yucong Jin <yucongj@microsoft.com>
2018-04-16 02:21:59 -0700

    remove logs

--- projects/sodexo/ui/stories/custom/index.js
+++ projects/sodexo/ui/stories/custom/index.js
@@ -2,12 +2,8 @@ import _ from 'lodash';
 
 const customStoriesIndexesJSON = localStorage.getItem('customeStories.index');
 
-window.console.log(customStoriesIndexesJSON);
-
 const customStoriesIndexes = JSON.parse(customStoriesIndexesJSON);
 
 const customStories = _.map(customStoriesIndexes, index => JSON.parse(window.localStorage.getItem(`customeStories.${index}`)));
-
-window.console.log(customStories);
 
 export default customStories;
