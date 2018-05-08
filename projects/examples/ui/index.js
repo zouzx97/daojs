62699632511707b03ae966d02731728a9d4d9896
Yucong Jin <yucongj@microsoft.com>
2018-05-07 20:19:30 -0700

    dynamic router

--- projects/examples/ui/index.js
+++ projects/examples/ui/index.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import _ from 'lodash';
 import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 
 import Landing from './landing';
+
+const SodexoLayout = () => (
+  <h1>Sodexo</h1>
+);
 
 const examples = [
   {
     name: 'sodexo',
     description: '描述sodexo项目',
     coverImage: '/img/sodexo.png',
+    component: SodexoLayout,
   },
 ];
-
-const SodexoLayout = () => (
-  <h1>Sodexo</h1>
-);
 
 ReactDOM.render(
   <HashRouter >
     <Switch>
-      <Route path="/sodexo" component={SodexoLayout} />
+      {_.map(examples, example => <Route key={example.name} path={`/${example.name}`} component={example.component} />)}
       <Route path="/all" component={() => <Landing examples={examples} />} />
       <Route component={() => <Redirect to="/all" />} />
     </Switch>
