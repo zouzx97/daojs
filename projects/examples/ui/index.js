7d923a4c93fa4849fa51633e672c4a48bd996857
Yucong Jin <yucongj@microsoft.com>
2018-05-08 02:26:47 -0700

    refactor

--- projects/examples/ui/index.js
+++ projects/examples/ui/index.js
@@ -8,47 +8,7 @@ import 'antd/dist/antd.css';
 import Landing from './landing';
 import ExamplePage from './example-page';
 import ExampleStoriesList from './example-stories-list';
-
-import {
-  Fundamentals,
-  AIOperations,
-  DefaultStories,
-  CustomStories,
-} from './stories/index';
-
-const rawExamples = [
-  {
-    name: 'sodexo',
-    description: '描述sodexo项目',
-    coverImage: '/img/sodexo.png',
-    logoImage: '/img/sodexo.logo.png',
-    categories: [{
-      id: '35a62e19-4b1b-4541-85aa-aa86b56c44ae',
-      name: '基础面板',
-      stories: Fundamentals,
-    }, {
-      id: 'b115caf6-620f-4a07-9cff-7f813d7e2c5f',
-      name: '智能运营助手',
-      stories: AIOperations,
-    }],
-  },
-  {
-    name: 'demo',
-    description: 'demo项目',
-    coverImage: '/img/demo.png',
-    logoImage: '/img/demo.png',
-    categories: [{
-      id: '35a62e19-4b1b-4541-85aa-aa86b56c44ae',
-      name: 'Default stories',
-      stories: DefaultStories,
-    }, {
-      id: '0dabd0a2-1124-54a3-98dc-685aa110d129',
-      name: 'Custom stories',
-      stories: CustomStories,
-      isStoryEditable: true,
-    }],
-  },
-];
+import rawExamples from './examples-list';
 
 const examples = _.map(rawExamples, rawExample => _.defaults({}, rawExample, {
   pageComponent() {
