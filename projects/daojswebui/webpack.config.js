0874efd0744ad999bb3b4757d125abe1079d2c4a
suro1995 <lxdyran@qq.com>
2018-05-14 22:37:33 +0800

    monaco editor

--- projects/daojswebui/webpack.config.js
+++ projects/daojswebui/webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-// const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -68,6 +68,11 @@ module.exports = {
     module: 'empty',
   },
   plugins: [
-    // new MonacoWebpackPlugin(),
+    new CopyWebpackPlugin([
+      {
+        from: 'node_modules/monaco-editor/min/vs',
+        to: 'vs',
+      },
+    ]),
   ],
 };
