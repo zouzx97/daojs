b8872b3c9053a602d61612c2b1db527ef455457f
Yucong Jin <yucongj@microsoft.com>
2018-05-15 00:10:40 -0700

    fix slice

--- projects/daojswebui/server.js
+++ projects/daojswebui/server.js
@@ -11,17 +11,19 @@ const router = new Router();
 
 
 router.post('/insight', koaBody(), async (ctx, next) => {
-  const { body, query } = ctx.request;
-  console.log(ctx.request.query);
+  const { body, query, headers } = ctx.request;
+  console.log(headers);
 
   if (cacheManager.isInCache(body)) {
     ctx.body = cacheManager.readFromCache(body);
   } else {
-    console.log(ctx.request.query);
-
     const result = await rp.post({
       uri: `http://52.175.225.59/api/insight?type=${query.type}`,
       json: true,
+      headers: {
+        Customer: headers.customer,
+        Business: headers.business,
+      },
       body,
     });
 
