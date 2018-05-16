50ba6112ba96b9b171bd57c68da9db9a49ce64a9
suro1995 <lxdyran@qq.com>
2018-05-16 20:14:37 +0800

    comp2

--- packages/builtin-components/demo/demo/comp2.demo.js
+++ packages/builtin-components/demo/demo/comp2.demo.js
@@ -8,19 +8,21 @@ const {
 
 export default function Comp2Demo() {
   return (
-    <GridLayout>
-      <div style={{ width: '300px', height: '130px', margin: '20px' }} >
-        <Comp2 title="2562" subTitle="Total Sales today" icon="share-alt" />
-      </div>
-      <div style={{ width: '300px', height: '130px', margin: '20px' }} >
-        <Comp2 title="5685" subTitle="Daily visitors" icon="shopping-cart" />
-      </div>
-      <div style={{ width: '300px', height: '130px', margin: '20px' }} >
-        <Comp2 title="12480" subTitle="Total Earning" icon="tag-o" />
-      </div>
-      <div style={{ width: '300px', height: '130px', margin: '20px' }} >
-        <Comp2 title="62" subTitle="Pending Orders" icon="star-o" />
-      </div>
-    </GridLayout>
+    <div style={{ background: '#F5F6F5' }}>
+      <GridLayout>
+        <div style={{ width: '300px', height: '130px', margin: '20px' }} >
+          <Comp2 title="2562" subTitle="Total Sales today" icon="share-alt" />
+        </div>
+        <div style={{ width: '300px', height: '130px', margin: '20px' }} >
+          <Comp2 title="5685" subTitle="Daily visitors" icon="shopping-cart" />
+        </div>
+        <div style={{ width: '300px', height: '130px', margin: '20px' }} >
+          <Comp2 title="12480" subTitle="Total Earning" icon="tag-o" />
+        </div>
+        <div style={{ width: '300px', height: '130px', margin: '20px' }} >
+          <Comp2 title="62" subTitle="Pending Orders" icon="star-o" />
+        </div>
+      </GridLayout>
+    </div>
   );
 }
