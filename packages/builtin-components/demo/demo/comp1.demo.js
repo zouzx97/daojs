89fd4527c100af287bb2625034a9d6f4c81feb01
suro1995 <lxdyran@qq.com>
2018-05-16 18:56:57 +0800

    demo

--- packages/builtin-components/demo/demo/comp1.demo.js
+++ packages/builtin-components/demo/demo/comp1.demo.js
@@ -3,12 +3,24 @@ import builtinComponents from '../../src/index';
 
 const {
   Comp1,
+  GridLayout,
 } = builtinComponents;
 
 export default function Comp1Demo() {
   return (
-    <div style={{ width: '400px', height: '200px' }}>
-      <Comp1 title="2562" subTitle="Total Sales today" percent={35} />
-    </div>
+    <GridLayout>
+      <div style={{ width: '300px', height: '100px', margin: '20px' }} >
+        <Comp1 title="2562" subTitle="Total Sales today" percent={35} />
+      </div>
+      <div style={{ width: '300px', height: '100px', margin: '20px' }} >
+        <Comp1 title="5685" subTitle="Daily visitors" percent={75} />
+      </div>
+      <div style={{ width: '300px', height: '100px', margin: '20px' }} >
+        <Comp1 title="12480" subTitle="Total Earning" percent={58} />
+      </div>
+      <div style={{ width: '300px', height: '100px', margin: '20px' }} >
+        <Comp1 title="62" subTitle="Pending Orders" percent={62} />
+      </div>
+    </GridLayout>
   );
 }
