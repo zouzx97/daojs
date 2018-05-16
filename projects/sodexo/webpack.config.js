68878b7fe5baca0a930ecc702a6db313c2d8ed6d
suro1995 <lxdyran@qq.com>
2018-05-16 11:15:45 +0800

    add less loader

--- projects/sodexo/webpack.config.js
+++ projects/sodexo/webpack.config.js
@@ -35,6 +35,16 @@ module.exports = {
         ],
       },
       {
+        test: /\.less$/,
+        use: [{
+          loader: 'style-loader', // creates style nodes from JS strings
+        }, {
+          loader: 'css-loader', // translates CSS into CommonJS
+        }, {
+          loader: 'less-loader', // compiles Less to CSS
+        }],
+      },
+      {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         use: 'babel-loader',
