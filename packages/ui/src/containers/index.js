03d529cc33a8b2ce134ab2a1d38c9935408110e6
HaipengWang <haip_wang@163.com>
2018-05-16 06:31:34 -0700

    update grid layout container

--- packages/ui/src/containers/index.js
+++ packages/ui/src/containers/index.js
@@ -1,11 +1,11 @@
 import AdjustableContainer from './adjustable-container';
 import CardContainer from './card-container';
 import FlexboxContainer from './flexbox-container';
-import GridLayout from './grid-layout';
+import GridLayoutContainer from './grid-layout';
 
 export default {
   AdjustableContainer,
   CardContainer,
   FlexboxContainer,
-  GridLayout,
+  GridLayoutContainer,
 };
