0aa6b216bab34bce0182383ded205a478441bcdc
suro1995 <lxdyran@qq.com>
2018-05-16 11:05:55 +0800

    ..

--- packages/builtin-components/src/components/mini-area.js
+++ packages/builtin-components/src/components/mini-area.js
@@ -42,7 +42,7 @@ export default class MiniArea extends React.Component {
       }),
     ];
 
-    const chartHeight = height + 43;
+    const chartHeight = height + 33;
 
     return (
       <div className={styles.miniChart} style={{ height }}>
