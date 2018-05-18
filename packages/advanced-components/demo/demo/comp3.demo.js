67dbabcbfef5d01c6a599a986fd00041863992de
suro1995 <lxdyran@qq.com>
2018-05-18 10:49:45 +0800

    comp3

--- packages/advanced-components/demo/demo/comp3.demo.js
+++ packages/advanced-components/demo/demo/comp3.demo.js
@@ -7,43 +7,18 @@ const { GridLayout } = builtinComponents;
 export default function Comp3Demo() {
   return (
     <GridLayout>
-      <div style={{ width: '300px', height: '130px', margin: '20px' }} >
-        <Comp3 title="2562" subTitle="Total Sales today" icon="share-alt" percent={50} footer="Better than last week (50%)" />
-      </div>
-      <div style={{ width: '300px', height: '130px', margin: '20px' }} >
-        <Comp3 title="5685" subTitle="Daily visitors" icon="shopping-cart" percent={45} footer="Better than last week (45%)" />
-      </div>
-      <div style={{ width: '300px', height: '130px', margin: '20px' }} >
-        <Comp3 title="12480" subTitle="Total Earning" icon="tag-o" percent={60} footer="Better than last week (60%)" />
-      </div>
-      <div style={{ width: '300px', height: '130px', margin: '20px' }} >
-        <Comp3 title="62" subTitle="Pending Orders" icon="star-o" percent={85} footer="Better than last week (85%)" />
-      </div>
-      <div style={{ width: '300px', height: '130px', margin: '20px' }} >
-        <Comp3 title="12480" subTitle="Total Earning" icon="tag-o" percent={60} footer="Better than last week (60%)" />
-      </div>
-      <div style={{ width: '300px', height: '130px', margin: '20px' }} >
-        <Comp3 title="62" subTitle="Pending Orders" icon="star-o" percent={85} footer="Better than last week (85%)" />
-      </div>
-
-      <div style={{ width: '500px', height: '160px', margin: '20px' }} >
-        <Comp3 title="62" subTitle="Pending Orders" icon="star-o" percent={85} footer="Better than last week (85%)" />
-      </div>
-      <div style={{ width: '500px', height: '160px', margin: '20px' }} >
-        <Comp3 title="12480" subTitle="Total Earning" icon="tag-o" percent={60} footer="Better than last week (60%)" />
-      </div>
-      <div style={{ width: '500px', height: '160px', margin: '20px' }} >
-        <Comp3 title="62" subTitle="Pending Orders" icon="star-o" percent={85} footer="Better than last week (85%)" />
-      </div>
-      <div style={{ width: '500px', height: '160px', margin: '20px' }} >
-        <Comp3 title="12480" subTitle="Total Earning" icon="tag-o" percent={60} footer="Better than last week (60%)" />
-      </div>
-      <div style={{ width: '500px', height: '160px', margin: '20px' }} >
-        <Comp3 title="62" subTitle="Pending Orders" icon="star-o" percent={85} footer="Better than last week (85%)" />
-      </div>
-      <div style={{ width: '500px', height: '160px', margin: '20px' }} >
-        <Comp3 title="12480" subTitle="Total Earning" icon="tag-o" percent={60} footer="Better than last week (60%)" />
-      </div>
+      <Comp3 title="2562" subTitle="Total Sales today" icon="share-alt" percent={50} footer="Better than last week (50%)" />
+      <Comp3 title="5685" subTitle="Daily visitors" icon="shopping-cart" percent={45} footer="Better than last week (45%)" />
+      <Comp3 title="12480" subTitle="Total Earning" icon="tag-o" percent={60} footer="Better than last week (60%)" />
+      <Comp3 title="62" subTitle="Pending Orders" icon="star-o" percent={85} footer="Better than last week (85%)" />
+      <Comp3 title="12480" subTitle="Total Earning" icon="tag-o" percent={60} footer="Better than last week (60%)" />
+      <Comp3 title="62" subTitle="Pending Orders" icon="star-o" percent={85} footer="Better than last week (85%)" />
+      <Comp3 title="62" subTitle="Pending Orders" icon="star-o" percent={85} footer="Better than last week (85%)" />
+      <Comp3 title="12480" subTitle="Total Earning" icon="tag-o" percent={60} footer="Better than last week (60%)" />
+      <Comp3 title="62" subTitle="Pending Orders" icon="star-o" percent={85} footer="Better than last week (85%)" />
+      <Comp3 title="12480" subTitle="Total Earning" icon="tag-o" percent={60} footer="Better than last week (60%)" />
+      <Comp3 title="62" subTitle="Pending Orders" icon="star-o" percent={85} footer="Better than last week (85%)" />
+      <Comp3 title="12480" subTitle="Total Earning" icon="tag-o" percent={60} footer="Better than last week (60%)" />
     </GridLayout>
   );
 }
--- packages/advanced-components/src/components/comp2.js
+++ packages/advanced-components/src/components/comp2.js
@@ -3,6 +3,7 @@ import _ from 'lodash';
 import ContainerDimensions from 'react-container-dimensions';
 import { ThemeContext } from '@daojs/contexts';
 import { Icon } from 'antd';
+import { createDecipheriv } from 'crypto';
 
 const padding = 15; // px
 
@@ -16,41 +17,31 @@ export default function Comp2(props) {
   const backgroundColor = '#fff';
 
   return (
-    <ThemeContext.Consumer>
-      {({ primaryColor }) => (
-        <ContainerDimensions>
-          { ({ width, height }) => {
-            const titleSize = _.round(width / 13);
-            const subTitleSize = _.round(width / 20);
-            const radius = _.min([
-              (width / 2) - padding,
-              (height - (2 * padding)),
-            ]);
+    <div className="card2">
+      <ThemeContext.Consumer>
+        {({ primaryColor }) => (
+          <ContainerDimensions>
+            { ({ width, height }) => {
+              const titleSize = _.round(width / 13);
+              const subTitleSize = _.round(width / 20);
+              const radius = _.min([
+                (width / 2) - padding,
+                (height - (2 * padding)),
+              ]);
 
-            return (
-              <div style={{
-                width: '100%',
-                height: '100%',
-                padding: `${padding}px`,
-                display: 'flex',
-                backgroundColor,
-                border: '2px solid #eee',
-                borderRadius: '5px',
-              }}
-              >
+              return (
                 <div style={{
-                  flex: 1,
+                  width: '100%',
+                  height: '100%',
+                  padding: `${padding}px`,
                   display: 'flex',
-                  flexDirection: 'row',
-                  justifyContent: 'center',
-                  alignItems: 'center',
+                  backgroundColor,
+                  border: '2px solid #eee',
+                  borderRadius: '5px',
                 }}
                 >
                   <div style={{
-                    width: `${radius}px`,
-                    height: `${radius}px`,
-                    borderRadius: `${radius}px`,
-                    backgroundColor: primaryColor,
+                    flex: 1,
                     display: 'flex',
                     flexDirection: 'row',
                     justifyContent: 'center',
@@ -58,51 +49,63 @@ export default function Comp2(props) {
                   }}
                   >
                     <div style={{
-                      width: `${_.round(radius * 0.8)}px`,
-                      height: `${_.round(radius * 0.8)}px`,
-                      borderRadius: `${_.round(radius * 0.8)}px`,
-                      backgroundColor: 'rgba(255, 255, 255, 0.2)',
+                      width: `${radius}px`,
+                      height: `${radius}px`,
+                      borderRadius: `${radius}px`,
+                      backgroundColor: primaryColor,
                       display: 'flex',
                       flexDirection: 'row',
                       justifyContent: 'center',
                       alignItems: 'center',
                     }}
                     >
-                      <Icon type={icon} style={{ fontSize: `${_.round(titleSize * 1.5)}px`, color: '#fff' }} />
+                      <div style={{
+                        width: `${_.round(radius * 0.8)}px`,
+                        height: `${_.round(radius * 0.8)}px`,
+                        borderRadius: `${_.round(radius * 0.8)}px`,
+                        backgroundColor: 'rgba(255, 255, 255, 0.2)',
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                      }}
+                      >
+                        <Icon type={icon} style={{ fontSize: `${_.round(titleSize * 1.5)}px`, color: '#fff' }} />
+                      </div>
+                    </div>
+                  </div>
+                  <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
+                    <div style={{
+                      color: '#797979',
+                      flex: 1,
+                      fontSize: `${titleSize}px`,
+                      display: 'flex',
+                      alignItems: 'flex-end',
+                      justifyContent: 'flex-end',
+                    }}
+                    >
+                      <div style={{ textAlign: 'right' }}>
+                        { title }
+                      </div>
+                    </div>
+                    <div style={{
+                      flex: 1,
+                      textAlign: 'right',
+                      color: '#7D8B96',
+                      fontSize: `${subTitleSize}px`,
+                    }}
+                    >
+                      { subTitle }
                     </div>
                   </div>
                 </div>
-                <div style={{ flex: 1, display: 'flex', flexDirection: 'column' }}>
-                  <div style={{
-                    color: '#797979',
-                    flex: 1,
-                    fontSize: `${titleSize}px`,
-                    display: 'flex',
-                    alignItems: 'flex-end',
-                    justifyContent: 'flex-end',
-                  }}
-                  >
-                    <div style={{ textAlign: 'right' }}>
-                      { title }
-                    </div>
-                  </div>
-                  <div style={{
-                    flex: 1,
-                    textAlign: 'right',
-                    color: '#7D8B96',
-                    fontSize: `${subTitleSize}px`,
-                  }}
-                  >
-                    { subTitle }
-                  </div>
-                </div>
-              </div>
-            );
-          }}
-        </ContainerDimensions>
-        )
-      }
-    </ThemeContext.Consumer>
+              );
+            }}
+          </ContainerDimensions>
+          )
+        }
+      </ThemeContext.Consumer>
+    </div>
   );
 }
 
