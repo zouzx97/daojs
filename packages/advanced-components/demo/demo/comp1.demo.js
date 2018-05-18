75d99932e6156192d6fb7422bed56bc734489a52
suro1995 <lxdyran@qq.com>
2018-05-18 10:31:36 +0800

    comp1

--- packages/advanced-components/demo/demo/comp1.demo.js
+++ packages/advanced-components/demo/demo/comp1.demo.js
@@ -7,40 +7,16 @@ const { GridLayout } = builtinComponents;
 export default function Comp1Demo() {
   return (
     <GridLayout>
-      <div style={{ width: '250px', height: '120px', margin: '10px' }} >
-        <Comp1 title="13432" subTitle="Total Sales today" percent={12} />
-      </div>
-      <div style={{ width: '250px', height: '120px', margin: '10px' }} >
-        <Comp1 title="52000" subTitle="Total Earning" percent={52} />
-      </div>
-      <div style={{ width: '250px', height: '120px', margin: '10px' }} >
-        <Comp1 title="1392" subTitle="Daily visitors" percent={62} />
-      </div>
-      <div style={{ width: '250px', height: '120px', margin: '10px' }} >
-        <Comp1 title="19" subTitle="Pending Orders" percent={100} />
-      </div>
-
-      <div style={{ width: '300px', height: '130px', margin: '20px' }} >
-        <Comp1 title="2562" subTitle="Total Sales today" percent={35} />
-      </div>
-      <div style={{ width: '300px', height: '130px', margin: '20px' }} >
-        <Comp1 title="5685" subTitle="Daily visitors" percent={75} />
-      </div>
-      <div style={{ width: '300px', height: '130px', margin: '20px' }} >
-        <Comp1 title="12480" subTitle="Total Earning" percent={58} />
-      </div>
-
-      <div style={{ width: '500px', height: '160px', margin: '20px' }} >
-        <Comp1 title="62" subTitle="Pending Orders" percent={62} />
-      </div>
-      <div style={{ width: '500px', height: '160px', margin: '20px' }} >
-        <Comp1 title="1024" subTitle="Finished Orders" percent={99} />
-      </div>
-
-      <div style={{ width: '800px', height: '220px', margin: '20px' }} >
-        <Comp1 title="10240" subTitle="Orders" percent={50} />
-      </div>
-
+      <Comp1 title="13432" subTitle="Total Sales today" percent={12} />
+      <Comp1 title="52000" subTitle="Total Earning" percent={52} />
+      <Comp1 title="1392" subTitle="Daily visitors" percent={62} />
+      <Comp1 title="19" subTitle="Pending Orders" percent={100} />
+      <Comp1 title="2562" subTitle="Total Sales today" percent={35} />
+      <Comp1 title="5685" subTitle="Daily visitors" percent={75} />
+      <Comp1 title="12480" subTitle="Total Earning" percent={58} />
+      <Comp1 title="62" subTitle="Pending Orders" percent={62} />
+      <Comp1 title="1024" subTitle="Finished Orders" percent={99} />
+      <Comp1 title="10240" subTitle="Orders" percent={50} />
     </GridLayout>
   );
 }
