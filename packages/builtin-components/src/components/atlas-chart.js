3aa419903c08a7ec05823a2dd2d6c7cf95d8b689
Yucong Jin <yucongj@microsoft.com>
2018-07-05 06:10:39 -0700

    atlas chart refactor

--- packages/builtin-components/src/components/atlas-chart.js
+++ packages/builtin-components/src/components/atlas-chart.js
@@ -3,97 +3,116 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import ReactEcharts from 'echarts-for-react';
 import { ThemeContext } from '@daojs/contexts';
+import {
+  compose,
+  setPropTypes,
+  defaultProps,
+  branch,
+  renderNothing,
+  withProps,
+} from 'recompose';
 
-export default class AtlasChart extends React.Component {
-  static propTypes = {
-    source: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.any)).isRequired,
-    axisDimensions: PropTypes.arrayOf(PropTypes.string),
-    metricDimensions: PropTypes.arrayOf(PropTypes.string),
-    title: PropTypes.string,
-    getOption: PropTypes.func.isRequired,
-    onEvents: PropTypes.objectOf(PropTypes.any),
-  }
+const fromRenderProps = (
+  RenderPropsComponent,
+  propsMapper,
+  renderPropName = 'children',
+) => (BaseComponent) => {
+  const baseFactory = React.createFactory(BaseComponent);
+  const renderPropsFactory = React.createFactory(RenderPropsComponent);
 
-  static defaultProps = {
-    axisDimensions: [],
-    metricDimensions: [],
-    title: null,
-    onEvents: {},
-  }
-
-  render() {
-    const {
-      source,
-      axisDimensions,
-      metricDimensions,
-      title,
-      restColors,
-    } = this.props;
-    if (_.isNil(source)) {
-      throw new Error('Chart source is nil');
-    }
-
-    if (_.isEmpty(source)) {
-      return null;
-    }
-
-    const dimensions = _.keys(_.head(source));
-
-    const axisDimension = _.head(axisDimensions)
-      || _.head(dimensions);
-
-    const axisData = _.map(source, (row) => {
-      const rawData = row[axisDimension];
-      if (axisDimension === 'timestamp' && _.isString(rawData)) {
-        return rawData.replace('T00:00:00Z', '').replace('T00:00:00.000Z', '');
-      }
-      return rawData;
+  const FromRenderProps = ownerProps =>
+    renderPropsFactory({
+      [renderPropName]: props =>
+        baseFactory({ ...ownerProps, ...propsMapper(props) }),
     });
 
-    const axisOption = {
-      data: axisData,
-      type: 'category',
-      boundaryGap: false,
-    };
+  return FromRenderProps;
+};
 
-    const metricDims = _.isEmpty(metricDimensions) ?
-      _.difference(dimensions, [axisDimension]) :
-      metricDimensions;
+const propTypes = {
+  source: PropTypes.arrayOf(PropTypes.objectOf(PropTypes.any)).isRequired,
+  axisDimensions: PropTypes.arrayOf(PropTypes.string),
+  metricDimensions: PropTypes.arrayOf(PropTypes.string),
+  title: PropTypes.string,
+  getOption: PropTypes.func.isRequired,
+  onEvents: PropTypes.objectOf(PropTypes.any),
+};
 
-    const titleOption = {
-      text: title,
-    };
+const atlasChartDefaultProps = {
+  axisDimensions: [],
+  metricDimensions: [],
+  title: null,
+  onEvents: {},
+};
 
-    const option = {
+const renderNothingIfNoSource = hasNoSource => branch(hasNoSource, renderNothing);
+
+const propsMapper = (props) => {
+  const {
+    source,
+    axisDimensions,
+    metricDimensions,
+    title,
+  } = props;
+
+  const dimensions = _.keys(_.head(source));
+
+  const axisDimension = _.head(axisDimensions)
+    || _.head(dimensions);
+
+  const axisData = _.map(source, (row) => {
+    const rawData = row[axisDimension];
+    if (axisDimension === 'timestamp' && _.isString(rawData)) {
+      return rawData.replace('T00:00:00Z', '').replace('T00:00:00.000Z', '');
+    }
+    return rawData;
+  });
+
+  const axisOption = {
+    data: axisData,
+    type: 'category',
+    boundaryGap: false,
+  };
+
+  const metricDims = _.isEmpty(metricDimensions) ?
+    _.difference(dimensions, [axisDimension]) :
+    metricDimensions;
+
+  const titleOption = {
+    text: title,
+  };
+
+  const option = {
+    titleOption,
+    backgroundColor: _.get(props, 'style.backgroundColor'),
+    ...props.getOption({
+      source,
+      dimensions,
+      axisDimension,
+      metricDimensions: metricDims,
+      axisData,
+      axisOption,
       titleOption,
-      backgroundColor: _.get(this.props, 'style.backgroundColor'),
-      ...this.props.getOption({
-        source,
-        dimensions,
-        axisDimension,
-        metricDimensions: metricDims,
-        axisData,
-        axisOption,
-        titleOption,
-      }),
-    };
+    }),
+  };
 
-    return (
-      <ThemeContext.Consumer>
-        {
-          ({ primaryColor }) => (
-            <ReactEcharts
-              theme="theme1"
-              option={primaryColor ? _.defaults({
-                color: [primaryColor, ...restColors || []],
-              }, option) : option}
-              notMerge={true} //eslint-disable-line
-              onEvents={this.props.onEvents}
-              {...this.props}
-            />
-          )
-        }
-      </ThemeContext.Consumer>
-    );
-  }
-}
+  const optionWithColor = props.primaryColor ? _.defaults({
+    color: [props.primaryColor, ...props.restColors || []],
+  }, option) : option;
+
+  return {
+    option: optionWithColor,
+    notMerge: true,
+    theme: 'theme1',
+  };
+};
+
+const enhance = compose(
+  setPropTypes(propTypes),
+  defaultProps(atlasChartDefaultProps),
+  renderNothingIfNoSource(({ source }) => _.isNil(source) || _.isEmpty(source)),
+  fromRenderProps(ThemeContext.Consumer, ({ primaryColor }) => ({ primaryColor })),
+  withProps(propsMapper),
+);
+
+export default enhance(ReactEcharts);
