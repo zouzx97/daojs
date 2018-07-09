84595497420124639f09a04716436fef10074024
Yucong Jin <yucongj@microsoft.com>
2018-07-08 22:11:28 -0700

    refactor spine

--- packages/builtin-components/demo/demo/fakeData.demo.js
+++ packages/builtin-components/demo/demo/fakeData.demo.js
@@ -64,17 +64,6 @@ fakeData.waterFall = [
 
 ];
 
-fakeData.spineData = [
-  ['Date', 'Income', 'Outcome'],
-  ['周一', 100, -50],
-  ['周二', 200, -180],
-  ['周三', 190, -89],
-  ['周四', 700, -605],
-  ['周五', 900, -590],
-  ['周六', 300, -200],
-  ['周日', 290, -100],
-];
-
 fakeData.wordCloud = [
   {
     "name": "life",
