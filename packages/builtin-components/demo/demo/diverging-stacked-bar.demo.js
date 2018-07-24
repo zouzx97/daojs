aa96ad6a028ea6058faadb5dfefb5638819bdf3d
zouzx97 <41323798+zouzx97@users.noreply.github.com>
2018-07-24 17:30:01 +0800

    packages/builtin-components/demo/demo/diverging-stacked-bar.demo.js

--- packages/builtin-components/demo/demo/diverging-stacked-bar.demo.js
+++ packages/builtin-components/demo/demo/diverging-stacked-bar.demo.js
@@ -6,10 +6,10 @@ const {
 } = builtinComponents;
 
 const source = [
-  {Date: '2017-10-24', Negative: 20, Neutral: 30, Positive: 10},
-  {Date: '2017-10-25', Negative: 40, Neutral: 35, Positive: 30},
-  {Date: '2017-10-26', Negative: 33, Neutral: 38, Positive: 30},
-  {Date: '2017-10-27', Negative: 40, Neutral: 40, Positive: 32},
+  { Date: '2017-10-24', Negative: 20, Neutral: 30, Positive: 10 },
+  { Date: '2017-10-25', Negative: 40, Neutral: 35, Positive: 30 },
+  { Date: '2017-10-26', Negative: 33, Neutral: 38, Positive: 30 },
+  { Date: '2017-10-27', Negative: 40, Neutral: 40, Positive: 32 },
 ];
 const sliceKey = [
   'Date', 'Negative', 'Neutral', 'Positive',
