587ba1bdc10ded7a465299cd79e8d9295c1cc1f2
zouzx97 <41323798+zouzx97@users.noreply.github.com>
2018-07-25 10:01:15 +0800

    funel.js

--- packages/builtin-components/demo/demo/funnel.demo.js
+++ packages/builtin-components/demo/demo/funnel.demo.js
@@ -10,16 +10,18 @@ const {
 } = builtinComponents;
 
 const data = [
-  ['name', 'value'],
-  ['访问', 60],
-  ['咨询', 40],
-  ['订单', 20],
-  ['点击', 80],
-  ['展现', 110],
+  {name: '访问', value: 60},
+  {name: '咨询', value: 40},
+  {name: '订单', value: 20},
+  {name: '点击', value: 80},
+  {name: '展现', value: 110},
+];
+const sliceKey = [
+  'name', 'value',
 ];
 
 export default function FunnelDemo() {
   return (
-    <Funnel source={data} />
+    <Funnel source={data} sliceKey={sliceKey} />
   );
 }
